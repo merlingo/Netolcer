7024c69b4a962abe8f74a1b2ede3c8bd9a81077e
merlingo <mrt.narr@gmail.com>
2017-06-01 17:44:55 +0300

    Parsing Text codes adding

--- Arayuz/Form1.cs
+++ Arayuz/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Baglanti;
 using System.IO;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
